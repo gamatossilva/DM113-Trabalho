cc726ed6899c91439db6949203cda95b86c9beaf
Guilherme Augusto de Matos Silva <guilherme_amsilva@hotmail.com>
2019-12-11 20:55:14 -0300

    Implementing services

---
+++ ProdutosEstoqueService/IProdutosEstoqueService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ProdutosEstoque {
+
+    [ServiceContract]
+    public interface IProdutosEstoqueService {
+
+        [OperationContract]
+        List<String> ListarProdutos();
+
+        [OperationContract]
+        bool IncluirProduto(ProdutoEstoqueData produtoEstoqueData);
+
+        [OperationContract]
+        bool RemoverProduto(string removeProduto);
+
+        [OperationContract]
+        int ConsultarEstoque(string numeroProduto);
+
+        [OperationContract]
+        bool AdicionarEstoque(string numeroProduto, int quantidade);
+
+        [OperationContract]
+        bool RemoverEstoque(string numeroProduto, int quantidade);
+
+        [OperationContract]
+        ProdutoEstoqueData VerProduto(string numeroProduto);
+
+    }
+
+    [DataContract]
+    public class ProdutoEstoqueData {
+
+        [DataMember]
+        public string NumeroProduto;
+
+        [DataMember]
+        public string NomeProduto;
+
+        [DataMember]
+        public string DescricaoProduto;
+
+        [DataMember]
+        public int EstoqueProduto;
+
+    }
+
+}
