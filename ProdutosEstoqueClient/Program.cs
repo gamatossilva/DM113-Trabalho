37803667da80a7ccc19465ebfc31c54a73ed431f
Guilherme Augusto de Matos Silva <guilherme_amsilva@hotmail.com>
2019-12-12 20:53:02 -0300

    Implementing client

---
+++ ProdutosEstoqueClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ProdutosEstoqueClient.ProdutosEstoqueService;
+
+namespace ProdutosEstoqueClient {
+    class Program {
+        static void Main(string[] args) {
+            ProdutosEstoqueServiceClient produtosEstoqueClient = new ProdutosEstoqueServiceClient();
+            Console.WriteLine("Teste 1: Listar todos os produtos");
+
+            List<string> produtosEstoque = produtosEstoqueClient.ListarProdutos().ToList();
+            foreach(string s in produtosEstoque) {
+                Console.WriteLine("Produto: " + s);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProdutosEstoqueClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("ProdutosEstoqueClient")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProdutosEstoqueClient")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("12ce5273-5fe0-4c16-bcc7-6572a6dce1ac")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
